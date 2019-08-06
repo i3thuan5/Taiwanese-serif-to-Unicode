672795f714fea288cae1852b2fedab6a37d7d1e9
niauah <fernandochen799@gmail.com>
2019-08-06 14:35:33 +0800

    改資料kiap、解決import掠無csv的問題

--- TP2Unicode/轉換器.py
+++ TP2Unicode/轉換器.py
@@ -1,8 +1,9 @@
 import csv
+from os.path import join, dirname, abspath
 
 
 def 轉換(text):
-    with open('unicode.csv') as csvfile:
+    with open(join(dirname(abspath(__file__)), 'data', 'unicode.csv')) as csvfile:
         對應表 = csv.DictReader(csvfile)
         for row in 對應表:
             text = text.replace(chr(int(row['原始編碼'], 16)), row['對應編碼'])
